912700bd683d701375f817741733d272191d11eb
nicktischmak <nicktischmak@gmail.com>
2025-01-27 21:40:50 -0700

    header and footer

---
+++ public/src/Components/Footer/index.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="bg-cyan-300/75 text-neutral-400 font-serif">
+            <div className="text-center">
+                <h4>Xavier Cerna, Logan Smith, Nick Tischmak, Morgan Troper</h4>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ public/src/Components/Header/index.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header className="bg-cyan-500/75 text-neutral-400 font-serif flex-row align-center">
+            <div className="flex-row justify-space-between justify-center align-center">
+                <h1>TeeOff</h1>
+                <p>Select a course and get ready to tee off</p>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
